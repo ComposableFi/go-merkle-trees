49ec1620ddbe3aa3202eda0249d25a4238df66b4
Osho Emmanuel <oshoklinsmann@gmail.com>
2022-01-10 13:10:17 +0100

    return uint64 type from Push method instead of an interface (#13)
    * rename hash field in Leaf struct to Leaf
    * remove return parameter from commit method

--- mmr/log.go
+++ mmr/log.go
@@ -7,6 +7,8 @@ type logger interface {
 
 var log logger
 
+// UseLogger takes any type that satisfies the logger interface as a argument. It the sets the global log variable to the
+// the value passed as an argument.
 func UseLogger(l logger) {
 	log = l
 }
--- mmr/util.go
+++ mmr/util.go
@@ -4,27 +4,32 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
-type MemStore map[uint64]interface{}
+// MemStore is a map of bytes with uint64 values as its key
+type MemStore map[uint64][]byte
 
+// NewMemStore creates an returns a map of the MemStore type
 func NewMemStore() MemStore {
-	return make(MemStore, 0)
+	return make(MemStore)
 }
 
-func (m MemStore) append(pos uint64, elem []interface{}) {
+func (m MemStore) append(pos uint64, elem [][]byte) {
 	for index, value := range elem {
 		m[pos+uint64(index)] = value
 	}
 }
 
-func (m MemStore) getElem(pos uint64) interface{} {
+func (m MemStore) getElem(pos uint64) []byte {
 	return m[pos]
 }
 
+// MemMMR is MMR implementation that uses a memory store. It has the fields store which takes the MemStore as an argument
+// and the mmrSize of the tree.
 type MemMMR struct {
 	store   MemStore
 	mmrSize uint64
 }
 
+// NewMemMMR returns an object of the MemMMR type. It takes in the mmrSize and Memory Store (MemStore) as arguments.
 func NewMemMMR(mmrSize uint64, store MemStore) *MemMMR {
 	return &MemMMR{
 		mmrSize: mmrSize,
@@ -32,36 +37,43 @@ func NewMemMMR(mmrSize uint64, store MemStore) *MemMMR {
 	}
 }
 
+// Store returns the MemStore
 func (m *MemMMR) Store() MemStore {
 	return m.store
 }
 
+// GetRoot returns the root of the MMR tree
 func (m *MemMMR) GetRoot() (interface{}, error) {
 	merge := &Merge{}
 	mmr := NewMMR(m.mmrSize, m.store, merge)
 	return mmr.GetRoot()
 }
 
-func (m *MemMMR) Push(elem interface{}) interface{} {
+// Push adds an element to the store and returns the position of the element
+func (m *MemMMR) Push(elem []byte) uint64 {
 	merge := &Merge{}
 	mmr := NewMMR(m.mmrSize, m.store, merge)
 	pos, err := mmr.Push(elem)
 	if err != nil {
 		log.Error(err.Error())
-		return nil
+		return 0
 	}
 	mmr.Commit()
 	return pos
 }
 
+// GenProof generates proofs for validating an MMR leaf
 func (m *MemMMR) GenProof(posList []uint64) (*MerkleProof, error) {
 	merge := &Merge{}
 	mmr := NewMMR(m.mmrSize, m.store, merge)
 	return mmr.GenProof(posList)
 }
 
+// Merge is an empty struct that satisfies the Merge interface by implementing the Merge method.
 type Merge struct{}
 
+// Merge takes two arguments of type []byte, appends both into a single byte slice, hashes the result using blake2b
+// and returns the resultant hash.
 func (m *Merge) Merge(left, right interface{}) interface{} {
 	l := left.([]byte)
 	r := right.([]byte)
