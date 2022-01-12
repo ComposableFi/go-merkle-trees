829baed4a40c2a726aff54f739a92ce5c752ee10
Osho Klinsmann <oshoklinsmann@gmail.com>
2022-01-12 08:18:36 +0100

    implement test suite from snowbridge ethereum mmr verification

--- mmr/log.go
+++ mmr/log.go
@@ -12,3 +12,16 @@ var log logger
 func UseLogger(l logger) {
 	log = l
 }
+
+type logg struct{}
+
+// Errorf has no log body, it simply satisfies the logger interface
+func (l logg) Errorf(format string, params ...interface{}) {}
+
+// Error has no log body, it simply satisfies the logger interface
+func (l logg) Error(params ...interface{}) {}
+
+func init() {
+	// set log to default logger which prints nothing if no logger interface is passed to the UseLogger method
+	log = logg{}
+}
