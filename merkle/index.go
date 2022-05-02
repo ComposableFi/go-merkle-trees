b04f096fa6426c91fe38bb735853ded4464ab2e8
vjdmhd <6095314+vjdmhd@users.noreply.github.com>
2022-05-02 14:09:46 +0430

    fix: rename idxs

--- merkle/index.go
+++ merkle/index.go
@@ -1,31 +1,31 @@
 package merkle
 
 // siblingIndecies returns indecies of sibling elements
-func siblingIndecies(idxs []uint64) []uint64 {
+func siblingIndecies(leafIndices []uint64) []uint64 {
 
-	indicesCount := len(idxs)
+	indicesCount := len(leafIndices)
 	siblings := make([]uint64, indicesCount)
 
 	// append all sibling indices to result
 	for i := 0; i < indicesCount; i++ {
-		siblings[i] = siblingIndex(idxs[i])
+		siblings[i] = siblingIndex(leafIndices[i])
 	}
 
 	return siblings
 }
 
 // parentIndecies returns indecies of parent elements
-func parentIndecies(idxs []uint64) []uint64 {
+func parentIndecies(leafIndices []uint64) []uint64 {
 
 	var parents []uint64
 
 	// loop through all indices
-	for i := 0; i < len(idxs); i++ {
+	for i := 0; i < len(leafIndices); i++ {
 
 		// get parent index
-		parentIndex := parentIndex(idxs[i])
+		parentIndex := parentIndex(leafIndices[i])
 
-		// checl if it is duplicate
+		// check if it is duplicate
 		isDuplicate := false
 		for j := 0; j < len(parents); j++ {
 			if parentIndex == parents[j] {
@@ -44,38 +44,38 @@ func parentIndecies(idxs []uint64) []uint64 {
 }
 
 // siblingIndex returns index of a sibling element
-// ex. idx = 100 then the bitwise operation idx ^ 1 returns 01100101
+// ex. index = 100 then the bitwise operation index ^ 1 returns 01100101
 // which is the binary representation of 101 and 101 ^ 1 return 01100100
 // which is the binary representation of 100
-func siblingIndex(idx uint64) uint64 {
-	return idx ^ 1
+func siblingIndex(index uint64) uint64 {
+	return index ^ 1
 }
 
 // isEvenIndex returns true if the index is even
-// ex. idx = 100 then the bitwise operation idx&1 returns 0
+// ex. index = 100 then the bitwise operation index&1 returns 0
 // 1   => 00000001
 // 100 => 01100100
 // 101 => 01100101
 // so the 100&1 returns 0
-func isEvenIndex(idx uint64) bool {
-	return idx&1 == 0
+func isEvenIndex(index uint64) bool {
+	return index&1 == 0
 }
 
 // getLeftIndex returns the left node index using bitwise operation
 // the left index is multiply by 2
-func getLeftIndex(idx int) int {
-	return idx << 1
+func getLeftIndex(index int) int {
+	return index << 1
 }
 
 // getRightIndex returns right node index, this is next index the left index
-func getRightIndex(idx int) int {
-	return getLeftIndex(idx) + 1
+func getRightIndex(index int) int {
+	return getLeftIndex(index) + 1
 }
 
 // parentIndex returns index of a parent element
 // the half of sibling index is the parent node index
-func parentIndex(idx uint64) uint64 {
-	return siblingIndex(idx) / 2
+func parentIndex(index uint64) uint64 {
+	return siblingIndex(index) / 2
 }
 
 // extractNewIndicesFromSiblings finds the sibling indices which is not present in leaf indices
